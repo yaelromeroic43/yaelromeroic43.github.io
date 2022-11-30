9cf83fd0baa92e3e8c460a45f39edc33174d611e
yaelromeroic43 <119464915+yaelromeroic43@users.noreply.github.com>
2022-11-29 19:21:30 -0600

    Create ctrlMascotas.js

---
+++ js/ctrlMascotas.js
@@ -0,0 +1,93 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  cod,
+  muestraError
+} from "../lib/util.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+/** @type {HTMLUListElement} */
+const lista = document.
+  querySelector("#lista");
+const daoMascota =
+  getFirestore().
+    collection("Mascota");
+
+getAuth().
+  onAuthStateChanged(
+    protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    consulta();
+  }
+}
+
+function consulta() {
+  daoMascota.
+    orderBy("nombre")
+    .onSnapshot(
+      htmlLista, errConsulta);
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    QuerySnapshot} snap */
+function htmlLista(snap) {
+  let html = "";
+  if (snap.size > 0) {
+    snap.forEach(doc =>
+      html += htmlFila(doc));
+  } else {
+    html += /* html */
+      `<li class="vacio">
+        -- No hay mascotas
+        registradas. --
+      </li>`;
+  }
+  lista.innerHTML = html;
+}
+
+/**
+ * @param {import(
+    "../lib/tiposFire.js").
+    DocumentSnapshot} doc */
+function htmlFila(doc) {
+  /**
+   * @type {import("./tipos.js").
+                  Alumno} */
+  const data = doc.data();
+  const nombre = cod(data.nombre);
+  var fsf= cod(data.fecha);
+  var fecha = new Date(fsf);
+  var espacio="[   -   ]";
+  var dformat = [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join('/');
+  const parámetros =
+    new URLSearchParams();
+  parámetros.append("id", doc.id);
+  return ( /* html */
+    `<li>
+      <a class="fila" href=
+  "alumno.html?${parámetros}">
+        <strong class="primario">
+          ${nombre} ${edad} ${dformat}
+        </strong>
+      </a>
+     
+    </li>`);
+}
+
+/** @param {Error} e */
+function errConsulta(e) {
+  muestraError(e);
+  consulta();
+}
